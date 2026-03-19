0cecc9c5dbabcf95f05ed1f9a4234eb61f899628
Danighoul9 <danielddc270606@gmail.com>
2026-03-19 16:42:08 +0100

    a

---
+++ src/Tema7/Comparable/App.java
@@ -0,0 +1,32 @@
+package Tema7.Comparable;
+
+import java.util.*;
+
+public class App {
+    static void main(String[] args) {
+        List<Personaje> pj = new ArrayList<>();
+        pj.add(new Personaje("Gandalf","Demonio",5,50));
+        pj.add(new Personaje("Legolas","Guerrero",10,90));
+        pj.add(new Personaje("Asgorn","Hechicero",9,100));
+        pj.add(new Personaje("Dante","Espadachin",25,210));
+
+        //Ordenar el ArrayList por lo que indiqaue el Comprabale Personaje -> por nombre
+        Collections.sort(pj);
+
+        for (Personaje personaje : pj){
+            IO.println(personaje);
+        }
+
+
+        IO.println("-----------------------------------------------------------");
+        Set<Personaje> pjOrdenados = new TreeSet<>();
+        //En los Set ojo con Comparable, si se repite el elemento de ordenación no lo añade
+        pjOrdenados.add(new Personaje("Gandalf","Demonio",5,50));
+        pjOrdenados.add(new Personaje("Legolas","Guerrero",10,90));
+        pjOrdenados.add(new Personaje("Asgorn","Hechicero",9,100));
+        pjOrdenados.add(new Personaje("Dante","Espadachin",25,210));
+        for (Personaje pjOrd :pjOrdenados){
+            IO.println(pjOrd);
+        }
+    }
+}
