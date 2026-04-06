f0c7aaaa51911d95b7e22a3a7502c33cf96aa975
alumno03 <danielddc270606@gmail.com>
2026-04-06 19:35:46 +0200

    a

---
+++ src/Tema7/Tema7_I/Practica1Tema7/Entities/Actividad.java
@@ -0,0 +1,33 @@
+package Tema7.Tema7_I.Practica1Tema7.Entities;
+
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalTime;
+import java.util.Comparator;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "codigo")
+
+public class Actividad implements Comparable<Actividad> {
+
+    private Integer codigo;
+    private String nombre;
+    private String descripcion;
+    private diaSemana diaSemana;
+    private LocalTime horaInicio;
+    private Integer duracionMinutos;
+    private Integer aforoMaximo;
+    private String monitor;
+
+
+    @Override
+    public int compareTo(@NotNull Actividad actividad) {
+        return Comparator.comparing(Actividad::getDiaSemana)
+                .thenComparing(Actividad::getHoraInicio)
+                .compare(this, actividad);
+    }
+}
---
+++ src/Tema7/Tema7_I/Practica1Tema7/Entities/Reserva.java
@@ -0,0 +1,35 @@
+package Tema7.Tema7_I.Practica1Tema7.Entities;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"socio", "actividad", "fechaReserva"})
+public class Reserva {
+
+    private Socio socio;
+    private Actividad actividad;
+    private LocalDate fechaReserva;
+    private Boolean asistio = false;
+
+    public boolean confirmarAsistencia() {
+        if (this.asistio == false) {
+            this.asistio = true;
+        }
+        return asistio;
+    }
+
+    public boolean estaActiva(){
+        if(fechaReserva.isAfter(LocalDate.now()) || fechaReserva.isEqual(LocalDate.now())) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+}
